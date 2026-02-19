b438cf5426fd04df2915fe4db911ff9ea948a375
HitenR123 <pranjalkeshari2433@gmail.com>
2026-02-19 20:29:37 +0530

    update

--- cart.js
+++ cart.js
@@ -107,7 +107,7 @@ function checkout() {
 
   localStorage.setItem("pendingOrders", JSON.stringify(pending));
 
-  fetch("https://raw.githubusercontent.com/hitenr123/hostel-market/refs/heads/main/products.json", {
+  fetch("https://localhost:5000/update-orders", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(cart),
